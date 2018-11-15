2a28f69db9f15b83262fa0f2bde8ed4b7ab3717d
Arkadiusz Radziewicz <arkadiusz.radziewicz@bitbag.pl>
2018-11-15 18:38:52 +0100

    fix namespaces

---
+++ src/SetonoSyliusFacebookTrackingPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusFacebookTrackingPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SetonoSyliusFacebookTrackingPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
