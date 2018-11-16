10af8b42bf419207090208574cd48e697b29785a
Natalia Adamek <nataliaadamek@pc345.home>
2018-11-16 19:27:45 +0100

    add grids, add menu, fix routes

---
+++ src/Menu/FacebookTrackingMenuBuilder.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusFacebookTrackingPlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class FacebookTrackingMenuBuilder
+{
+    public function addFacebookTrackingItem(MenuBuilderEvent $event): void
+    {
+        $catalogMenu = $event->getMenu()->getChild('catalog');
+
+        $catalogMenu
+            ->addChild('facebook_tracking', ['route' => 'setono_sylius_facebook_tracking_plugin_admin_facebook_config_index'])
+            ->setLabel('setono_sylius_facebook_tracking_plugin.ui.facebook_config_index')
+            ->setLabelAttribute('icon', 'bullhorn')
+        ;
+    }
+}
