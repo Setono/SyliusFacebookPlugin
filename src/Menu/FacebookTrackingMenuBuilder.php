dd803c93ef09cab0be31c317ecb75940e6cc4197
Natalia Adamek <nataliaadamek@pc345.home>
2018-11-22 17:35:39 +0100

    fix admin && add services

--- src/Menu/FacebookTrackingMenuBuilder.php
+++ src/Menu/FacebookTrackingMenuBuilder.php
@@ -12,18 +12,13 @@ final class FacebookTrackingMenuBuilder
     /** @var FacebookConfigContextInterface */
     private $facebookConfigContext;
 
-    public function __construct(FacebookConfigContextInterface $facebookConfigContext)
-    {
-        $this->facebookConfigContext = $facebookConfigContext;
-    }
     public function addFacebookTrackingItem(MenuBuilderEvent $event): void
     {
         $catalogMenu = $event->getMenu()->getChild('catalog');
 
         $catalogMenu
             ->addChild('facebook_tracking', [
-                'route' => 'setono_sylius_facebook_tracking_plugin_admin_facebook_config_index',
-                'routeParameters' => ['id' => $this->facebookConfigContext->getConfig()->getId()],
+                'route' => 'setono_sylius_facebook_tracking_plugin_admin_facebook_config_index'
                 ])
             ->setLabel('setono_sylius_facebook_tracking_plugin.ui.facebook_config_index')
             ->setLabelAttribute('icon', 'bullhorn')
