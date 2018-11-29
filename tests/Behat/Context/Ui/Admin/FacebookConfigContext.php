ee6caac5b746a506f33da41b5f67163e63c50a0b
Arkadiusz Radziewicz <arkadiusz.radziewicz@bitbag.pl>
2018-11-29 18:42:48 +0100

    add behat, adjust translation, fix most of events not working

--- tests/Behat/Context/Ui/Admin/FacebookConfigContext.php
+++ tests/Behat/Context/Ui/Admin/FacebookConfigContext.php
@@ -6,48 +6,69 @@ namespace Tests\Setono\SyliusFacebookTrackingPlugin\Behat\Context\Ui\Admin;
 
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
+use Setono\SyliusFacebookTrackingPlugin\Context\FacebookConfigContextInterface;
+use Sylius\Behat\NotificationType;
+use Sylius\Behat\Service\NotificationCheckerInterface;
+use Tests\Setono\SyliusFacebookTrackingPlugin\Behat\Page\Admin\FacebookConfig\UpdatePageInterface;
 
 final class FacebookConfigContext implements Context
 {
+    /** @var UpdatePageInterface */
+    private $updatePage;
 
+    /** @var NotificationCheckerInterface */
+    private $notificationChecker;
+
+    /** @var FacebookConfigContextInterface */
+    private $facebookConfigContext;
+
+    public function __construct(
+        UpdatePageInterface $updatePage,
+        NotificationCheckerInterface $notificationChecker,
+        FacebookConfigContextInterface $facebookConfigContext
+    ) {
+        $this->updatePage = $updatePage;
+        $this->notificationChecker = $notificationChecker;
+        $this->facebookConfigContext = $facebookConfigContext;
+    }
 
     /**
      * @When I go to the create config page
      */
-    public function iGoToTheCreateConfigPage()
+    public function iGoToTheCreateConfigPage(): void
     {
-        throw new PendingException();
+        $this->updatePage->open(['id' => $this->facebookConfigContext->getConfig()->getId()]);
     }
 
     /**
-     * @When I fill the code with :arg1
+     * @When I fill the code with :code
      */
-    public function iFillTheCodeWith($arg1)
+    public function iFillTheCodeWith(string $code): void
     {
-        throw new PendingException();
+        $this->updatePage->fillCode($code);
     }
 
     /**
-     * @When I name it :arg1
+     * @When I name it :name
      */
-    public function iNameIt($arg1)
+    public function iNameIt(string $name): void
     {
-        throw new PendingException();
+        $this->updatePage->fillName($name);
     }
 
     /**
      * @When I add it
      */
-    public function iAddIt()
+    public function iAddIt(): void
     {
-        throw new PendingException();
+        $this->updatePage->saveChanges();
     }
 
     /**
      * @Then I should be notified that it has been successfully created
      */
-    public function iShouldBeNotifiedThatItHasBeenSuccessfullyCreated()
+    public function iShouldBeNotifiedThatItHasBeenSuccessfullyCreated(): void
     {
-        throw new PendingException();
+        $this->notificationChecker->checkNotification('has been successfully updated.', NotificationType::success());
     }
 }
--- tests/Behat/Page/Admin/FacebookConfig/UpdatePage.php
+++ tests/Behat/Page/Admin/FacebookConfig/UpdatePage.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 namespace Tests\Setono\SyliusFacebookTrackingPlugin\Behat\Page\Admin\FacebookConfig;
 
-final class UpdatePage
+use Sylius\Behat\Page\Admin\Crud\UpdatePage as BaseUpdatePage;
+
+final class UpdatePage extends BaseUpdatePage implements UpdatePageInterface
 {
+    public function fillCode(?string $code): void
+    {
+        $this->getDocument()->fillField('facebook_config_pixelCode', $code);
+    }
+
+    public function fillName(?string $name): void
+    {
+        $this->getDocument()->fillField('facebook_config_translations_en_US_name', $name);
+    }
 
 }
