9155c9013a3098d64231854fd43519fa3de9583e
Arkadiusz Radziewicz <arkadiusz.radziewicz@bitbag.pl>
2018-12-19 17:08:43 +0100

    [WIP] change services from YML to XML

--- src/DependencyInjection/SetonoSyliusFacebookTrackingExtension.php
+++ src/DependencyInjection/SetonoSyliusFacebookTrackingExtension.php
@@ -7,16 +7,16 @@ namespace Setono\SyliusFacebookTrackingPlugin\DependencyInjection;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class SetonoSyliusFacebookTrackingExtension extends AbstractResourceExtension
 {
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $loader->load('services.yml');
+        $loader->load('services.xml');
 
         $this->registerResources('setono_sylius_facebook_tracking', $config['driver'], $config['resources'], $container);
     }
