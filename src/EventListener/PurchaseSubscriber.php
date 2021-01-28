db1892b868cf29ac3e4562d4f23a69cfffac431e
Joachim Løvgaard <joachim@loevgaard.dk>
2021-01-28 14:34:57 +0100

    Track the purchase on the thank you page instead of the complete event

--- src/EventListener/PurchaseSubscriber.php
+++ src/EventListener/PurchaseSubscriber.php
@@ -4,32 +4,76 @@ declare(strict_types=1);
 
 namespace Setono\SyliusFacebookPlugin\EventListener;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Setono\SyliusFacebookPlugin\Builder\ContentBuilder;
 use Setono\SyliusFacebookPlugin\Builder\PurchaseBuilder;
+use Setono\SyliusFacebookPlugin\Context\PixelContextInterface;
 use Setono\SyliusFacebookPlugin\Event\BuilderEvent;
 use Setono\SyliusFacebookPlugin\Tag\FbqTag;
 use Setono\SyliusFacebookPlugin\Tag\FbqTagInterface;
 use Setono\SyliusFacebookPlugin\Tag\Tags;
 use Setono\TagBag\Tag\TagInterface;
-use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Setono\TagBag\TagBagInterface;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Order\Repository\OrderRepositoryInterface;
+use Symfony\Bundle\SecurityBundle\Security\FirewallMap;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 final class PurchaseSubscriber extends TagSubscriber
 {
+    private OrderRepositoryInterface $orderRepository;
+
+    public function __construct(
+        TagBagInterface $tagBag,
+        PixelContextInterface $pixelContext,
+        EventDispatcherInterface $eventDispatcher,
+        RequestStack $requestStack,
+        FirewallMap $firewallMap,
+        OrderRepositoryInterface $orderRepository
+    ) {
+        parent::__construct($tagBag, $pixelContext, $eventDispatcher, $requestStack, $firewallMap);
+
+        $this->orderRepository = $orderRepository;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
-            'sylius.order.post_complete' => [
-                'track',
-            ],
+            KernelEvents::REQUEST => 'track',
         ];
     }
 
-    public function track(ResourceControllerEvent $event): void
+    public function track(RequestEvent $event): void
     {
-        $order = $event->getSubject();
+        $request = $event->getRequest();
 
-        if (!$order instanceof OrderInterface || !$this->isShopContext()) {
+        if (!$event->isMasterRequest() || !$this->isShopContext($request)) {
+            return;
+        }
+
+        if (!$request->attributes->has('_route')) {
+            return;
+        }
+
+        $route = $request->attributes->get('_route');
+        if ('sylius_shop_order_thank_you' !== $route) {
+            return;
+        }
+
+        $orderId = $request->getSession()->get('sylius_order_id');
+
+        if (null === $orderId) {
+            return;
+        }
+
+        $order = $this->orderRepository->find($orderId);
+        if (null === $order) {
+            return;
+        }
+
+        if (!$order instanceof OrderInterface) {
             return;
         }
 
