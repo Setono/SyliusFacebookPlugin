a05a7ce157d148df5cab8d7e97dea6559f6edf31
Igor Mukhin <igor.mukhin@gmail.com>
2021-11-04 17:05:21 +0200

    Improved: ViewCategory-related code to be more universal and extendable

--- src/DataMapper/ViewCategoryDataMapper.php
+++ src/DataMapper/ViewCategoryDataMapper.php
@@ -4,56 +4,33 @@ declare(strict_types=1);
 
 namespace Setono\SyliusFacebookPlugin\DataMapper;
 
+use Setono\SyliusFacebookPlugin\Data\ViewCategoryData;
 use Setono\SyliusFacebookPlugin\ServerSide\ServerSideEventInterface;
-use Sylius\Bundle\ResourceBundle\Grid\View\ResourceGridView;
-use Sylius\Component\Locale\Context\LocaleContextInterface;
 use Sylius\Component\Product\Model\ProductInterface;
 use Sylius\Component\Taxonomy\Model\TaxonInterface;
-use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
 use Webmozart\Assert\Assert;
 
 /* not final */ class ViewCategoryDataMapper implements DataMapperInterface
 {
-    protected LocaleContextInterface $localeContext;
-
-    protected TaxonRepositoryInterface $taxonRepository;
-
-    public function __construct(
-        LocaleContextInterface $localeContext,
-        TaxonRepositoryInterface $taxonRepository
-    ) {
-        $this->localeContext = $localeContext;
-        $this->taxonRepository = $taxonRepository;
-    }
-
     /**
-     * @psalm-assert-if-true ResourceGridView $source
+     * @psalm-assert-if-true ViewCategoryData $source
      */
     public function supports(object $source, ServerSideEventInterface $target, array $context = []): bool
     {
-        return $source instanceof ResourceGridView;
+        return $source instanceof ViewCategoryData;
     }
 
     public function map(object $source, ServerSideEventInterface $target, array $context = []): void
     {
         Assert::true($this->supports($source, $target, $context));
 
-        $request = $source->getRequestConfiguration()->getRequest();
-
         $customData = $target->getCustomData();
         $customData
             ->setContentType(ServerSideEventInterface::CONTENT_TYPE_PRODUCT)
-            ->setContentIds($this->getContentIds($source))
+            ->setContentIds($this->getContentIds($source->getProducts()))
         ;
 
-        /** @var string|null $slug */
-        $slug = $request->attributes->get('slug');
-        if (null === $slug) {
-            return;
-        }
-
-        $locale = $this->localeContext->getLocaleCode();
-        $taxon = $this->taxonRepository->findOneBySlug($slug, $locale);
+        $taxon = $source->getTaxon();
         if (null === $taxon) {
             return;
         }
@@ -83,15 +60,13 @@ use Webmozart\Assert\Assert;
     }
 
     /**
+     * @param ProductInterface[] $products
      * @psalm-return array<array-key, string>
      */
-    protected function getContentIds(ResourceGridView $gridView, int $max = 10): array
+    protected function getContentIds(array $products, int $max = 10): array
     {
-        $data = $gridView->getData();
-        Assert::isInstanceOf($data, \Traversable::class);
-
         return array_slice(array_filter(array_map(function (ProductInterface $product): string {
             return $product->getCode() ?? '';
-        }, iterator_to_array($data))), 0, $max);
+        }, $products)), 0, $max);
     }
 }
--- src/EventListener/ViewCategorySubscriber.php
+++ src/EventListener/ViewCategorySubscriber.php
@@ -4,9 +4,22 @@ declare(strict_types=1);
 
 namespace Setono\SyliusFacebookPlugin\EventListener;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Setono\SyliusFacebookPlugin\Context\PixelContextInterface;
+use Setono\SyliusFacebookPlugin\Data\ViewCategoryData;
+use Setono\SyliusFacebookPlugin\DataMapper\DataMapperInterface;
+use Setono\SyliusFacebookPlugin\Factory\PixelEventFactoryInterface;
+use Setono\SyliusFacebookPlugin\ServerSide\ServerSideEventFactoryInterface;
 use Setono\SyliusFacebookPlugin\ServerSide\ServerSideEventInterface;
 use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Sylius\Bundle\ResourceBundle\Grid\View\ResourceGridView;
+use Sylius\Component\Locale\Context\LocaleContextInterface;
+use Sylius\Component\Product\Model\ProductInterface;
+use Sylius\Component\Taxonomy\Model\TaxonInterface;
+use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
+use Symfony\Bundle\SecurityBundle\Security\FirewallMap;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Webmozart\Assert\Assert;
 
 /**
  * See https://developers.facebook.com/docs/marketing-api/audiences/guides/dynamic-product-audiences/#setuppixel
@@ -14,6 +27,35 @@ use Sylius\Bundle\ResourceBundle\Grid\View\ResourceGridView;
  */
 final class ViewCategorySubscriber extends AbstractSubscriber
 {
+    protected LocaleContextInterface $localeContext;
+
+    protected TaxonRepositoryInterface $taxonRepository;
+
+    public function __construct(
+        PixelContextInterface $pixelContext,
+        RequestStack $requestStack,
+        FirewallMap $firewallMap,
+        ServerSideEventFactoryInterface $serverSideFactory,
+        DataMapperInterface $dataMapper,
+        PixelEventFactoryInterface $pixelEventFactory,
+        EntityManagerInterface $entityManager,
+        LocaleContextInterface $localeContext,
+        TaxonRepositoryInterface $taxonRepository
+    ) {
+        parent::__construct(
+            $pixelContext,
+            $requestStack,
+            $firewallMap,
+            $serverSideFactory,
+            $dataMapper,
+            $pixelEventFactory,
+            $entityManager
+        );
+
+        $this->localeContext = $localeContext;
+        $this->taxonRepository = $taxonRepository;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -34,9 +76,43 @@ final class ViewCategorySubscriber extends AbstractSubscriber
             return;
         }
 
+        $viewCategoryData = new ViewCategoryData(
+            $this->getProducts($gridView),
+            $this->getTaxon($gridView),
+        );
+
         $this->generatePixelEvents(
-            $gridView,
+            $viewCategoryData,
             ServerSideEventInterface::CUSTOM_EVENT_VIEW_CATEGORY
         );
+    }
+
+    /**
+     * @return ProductInterface[]
+     */
+    protected function getProducts(ResourceGridView $gridView): array
+    {
+        $data = $gridView->getData();
+        Assert::isInstanceOf($data, \Traversable::class);
+
+        $result = iterator_to_array($data);
+        Assert::allIsInstanceOf($result, ProductInterface::class);
+
+        return $result;
+    }
+
+    protected function getTaxon(ResourceGridView $gridView): ?TaxonInterface
+    {
+        $request = $gridView->getRequestConfiguration()->getRequest();
+
+        /** @var string|null $slug */
+        $slug = $request->attributes->get('slug');
+        if (null === $slug) {
+            return null;
+        }
+
+        $locale = $this->localeContext->getLocaleCode();
+
+        return $this->taxonRepository->findOneBySlug($slug, $locale);
     }
 }
